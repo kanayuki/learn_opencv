7ab7b016b8de389c052ba3a597127370a26a7f01
kanayuki <kanayuki@163.com>
2022-11-13 04:03:14 +0800

    1104 trefoil.ipynb

---
+++ sudoku/line.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+import numpy as np
+from numpy import cos, sin, pi
+
+img = cv.imread('../Image/sudoku_001.png')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+canny = cv.Canny(gray, 50, 150)
+lines = cv.HoughLines(canny, 0.1, np.pi / 180, 150)
+for line in lines:
+    rho, theta = line[0]
+    if abs(theta - pi / 2) < 0.01 or theta == 0:
+        p = (rho * cos(theta), rho * sin(theta))
+        pt1 = (int(p[0] - 1000 * sin(theta)), int(p[1] - 1000 * cos(theta)))
+        pt2 = (int(p[0] + 1000 * sin(theta)), int(p[1] + 1000 * cos(theta)))
+        cv.line(img, pt1, pt2, (255, 0, 255), 2)
+        print(rho, theta)
+
+cv.imshow("sudoku", canny)
+cv.imshow("line", img)
+cv.waitKey()
